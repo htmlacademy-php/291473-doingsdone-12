eb71aa8319e3589ea391dd8e57a17e4761f2f8ba
Дмитрий Кречетников <dkrech07@gmail.com>
2021-04-23 23:16:46 +0500

    Выводит содержимое шаблонов

--- templates/main.php
+++ templates/main.php
@@ -1,65 +1,64 @@
-            <section class="content__side">
-                <h2 class="content__side-heading">Проекты</h2>
+<section class="content__side">
+    <h2 class="content__side-heading">Проекты</h2>
 
-                <nav class="main-navigation">
-                    <ul class="main-navigation__list">
-                    <?php foreach($projects as $project): ?>
-                        <li class="main-navigation__list-item">
-                            <a class="main-navigation__list-item-link" href="#"><?= $project ?></a>
-                            <span class="main-navigation__list-item-count"><?= count_tasks($tasks, $project) ?></span>
-                        </li>
-                    <?php endforeach; ?>
-                    </ul>
-                </nav>
+    <nav class="main-navigation">
+        <ul class="main-navigation__list">
+            <?php foreach ($projects as $project) : ?>
+                <li class="main-navigation__list-item">
+                    <a class="main-navigation__list-item-link" href="#"><?= $project ?></a>
+                    <span class="main-navigation__list-item-count"><?= count_tasks($tasks, $project) ?></span>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
 
-                <a class="button button--transparent button--plus content__side-button"
-                   href="pages/form-project.html" target="project_add">Добавить проект</a>
-            </section>
+    <a class="button button--transparent button--plus content__side-button" href="pages/form-project.html" target="project_add">Добавить проект</a>
+</section>
 
-            <main class="content__main">
-                <h2 class="content__main-heading">Список задач</h2>
+<main class="content__main">
+    <h2 class="content__main-heading">Список задач</h2>
 
-                <form class="search-form" action="index.php" method="post" autocomplete="off">
-                    <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+    <form class="search-form" action="index.php" method="post" autocomplete="off">
+        <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
 
-                    <input class="search-form__submit" type="submit" name="" value="Искать">
-                </form>
+        <input class="search-form__submit" type="submit" name="" value="Искать">
+    </form>
 
-                <div class="tasks-controls">
-                    <nav class="tasks-switch">
-                        <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
-                        <a href="/" class="tasks-switch__item">Повестка дня</a>
-                        <a href="/" class="tasks-switch__item">Завтра</a>
-                        <a href="/" class="tasks-switch__item">Просроченные</a>
-                    </nav>
+    <div class="tasks-controls">
+        <nav class="tasks-switch">
+            <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
+            <a href="/" class="tasks-switch__item">Повестка дня</a>
+            <a href="/" class="tasks-switch__item">Завтра</a>
+            <a href="/" class="tasks-switch__item">Просроченные</a>
+        </nav>
 
-                    <label class="checkbox">
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?= $show_complete_tasks == 1 ? 'checked' : '' ?>>
-                        <span class="checkbox__text">Показывать выполненные</span>
+        <label class="checkbox">
+            <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?= $show_complete_tasks == 1 ? 'checked' : '' ?>>
+            <span class="checkbox__text">Показывать выполненные</span>
+        </label>
+    </div>
+
+    <table class="tasks">
+        <?php foreach ($tasks as $task) : ?>
+            <?php
+            if ($task['completed'] == 'true' && $show_complete_tasks == 1) {
+                continue;
+            }
+            ?>
+            <tr class="tasks__item task <?= $task['completed'] == 'true' ? 'task--completed' : '' ?>">
+                <td class="task__select">
+                    <label class="checkbox task__checkbox">
+                        <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
+                        <span class="checkbox__text"><?= $task['task'] ?></span>
                     </label>
-                </div>
+                </td>
 
-                <table class="tasks">
-                    <?php foreach($tasks as $task): ?>
-                        <?php
-                            if ($task['completed'] == 'true' && $show_complete_tasks == 1) {
-                                continue;
-                            }
-                        ?>
-                        <tr class="tasks__item task <?= $task['completed'] == 'true' ? 'task--completed' : '' ?>">
-                            <td class="task__select">
-                                <label class="checkbox task__checkbox">
-                                    <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
-                                    <span class="checkbox__text"><?= $task['task'] ?></span>
-                                </label>
-                            </td>
+                <td class="task__file">
+                    <a class="download-link" href="#">Home.psd</a>
+                </td>
 
-                            <td class="task__file">
-                                <a class="download-link" href="#">Home.psd</a>
-                            </td>
-
-                            <td class="task__date"><?= $task['date'] ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
-            </main>
+                <td class="task__date"><?= $task['date'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</main>
