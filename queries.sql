b69ec11e452350a4f20ae140151b3b441e6cf45a
Дмитрий Кречетников <dkrech07@gmail.com>
2021-04-24 22:29:06 +0500

    Добавляет запросы

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 -- Добавляет пользователей;
 -- password_hash('Q4B4e6Ap', PASSWORD_DEFAULT); // $2y$10$feI4UmS8vwRUqjGuvBkNheLQ4eL5KI4PW8YN11SmhzRi9ELC0ehUq
 -- password_hash('gQHVnixF', PASSWORD_DEFAULT); // $2y$10$7uOaL/PzYWwnhT9Ly2mz9.6tjORSkt7D2RZZ/ZO8mlozZnpLcy9Dm
-INSERT INTO users (registration_date, email, user_name, password) VALUES ('2021-04-24 17:40:25', 'dkrech07@gmail.com', 'dkrech07', '$2y$10$feI4UmS8vwRUqjGuvBkNheLQ4eL5KI4PW8YN11SmhzRi9ELC0ehUq'),
+INSERT INTO users (registration_date, email, user_name, password) VALUES ('2021-04-24 17:40:25', 'dkrech07@gmail.com', 'dmitry', '$2y$10$feI4UmS8vwRUqjGuvBkNheLQ4eL5KI4PW8YN11SmhzRi9ELC0ehUq'),
                                                                          ('2021-03-20 12:30:05', 'larisa@gmail.com', 'larisa', '$2y$10$7uOaL/PzYWwnhT9Ly2mz9.6tjORSkt7D2RZZ/ZO8mlozZnpLcy9Dm');
 
 -- Добавляет список проектов;
@@ -15,3 +15,23 @@ INSERT INTO tasks (create_date, status, task_name, file_link, deadline, user_id,
                                                                                                     ('2021-04-22 14:00:40', 0, 'Собеседование в IT компании', '', '2021-06-27 14:00:40', 1, 1),
                                                                                                     ('2021-04-23 14:30:45', 0, 'Купить корм для кота', '', '2021-06-28 14:30:45', 2, 1),
                                                                                                     ('2021-04-24 15:00:50', 0, 'Заказать пиццу', '', '2021-06-28 15:00:50', 2, 1);
+
+
+
+-- Получает список проектов для конкретного пользователя (по логину);
+SELECT p.* FROM projects p INNER JOIN users u ON u.id = p.user_id WHERE user_name = 'dmitry';
+
+-- Получает список проектов для конкретного пользователя (по id);
+SELECT * FROM projects WHERE user_id = 1;
+
+-- Получает список задач для конкретного пользователя (по логину);
+SELECT t.* FROM tasks t INNER JOIN users u ON u.id = t.user_id WHERE user_name = 'dmitry';
+
+-- Получает список задач для конкретного пользователя (по id);
+SELECT * FROM tasks WHERE user_id = 1;
+
+-- Помечает задачу как выполненную (по id);
+UPDATE tasks SET status = 1 WHERE id = 2;
+
+-- Обновляет название задачи (по id);
+UPDATE tasks SET task_name = 'Новое название задачи' WHERE id = 2;
