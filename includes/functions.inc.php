30153e0c3a194ed1ffc80a4be78443bb126f5733
Дмитрий Кречетников <dkrech07@gmail.com>
2021-06-07 16:13:32 +0500

    Подствечивает просроченную задачу только если у нее есть дата

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -71,8 +71,8 @@ function get_task_time($task)
     $current_time = time();
     $task_time = strtotime($task['deadline']);
     $task_deadline = ($task_time - $current_time) / 3600;
-
-    if ($task_deadline <= 24) {
+    
+    if (isset($task['deadline']) && $task_deadline <= 24) {
         return 'task--important';
     }
 };
